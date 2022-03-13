17590501c72c0cbc90a7d2e15a8013e2bf6fc43a
instantoats <47441008+instantoats@users.noreply.github.com>
2022-03-12 17:22:35 -0800

    test

--- PartThree/NotePadApp.py
+++ PartThree/NotePadApp.py
@@ -1,18 +1,67 @@
-from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox, QFontDialog, QColorDialog
 import sys
+from PyQt6.QtGui import QFont
+from PyQt6.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+from PyQt6.QtCore import QFileInfo, Qt
 from NotePadDemo import Ui_MainWindow
 
 
+# Create new window
 class NotePadWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
 
-
+# triggers for when the toolbar buttons are used
+# actionSave, actionNew etc. are the names I used in the NotePadDemo.py file (and designer)
         self.actionSave.triggered.connect(self.save_file)
+        self.actionNew.triggered.connect(self.file_new)
+        self.actionOpen.triggered.connect(self.open_file)
+        self.actionPrint.triggered.connect(self.print_file)
+        self.actionPrint_Preview.triggered.connect(self.preview_dialog)
+        self.actionExport_PDF.triggered.connect(self.export_pdf)
+        self.actionClose.triggered.connect(self.exit_app)
 
+# these five are built in functions, so we trigger them without creating an accompanying function
+        self.actionUndo.triggered.connect(self.textEdit.undo)
+        self.actionRedo.triggered.connect(self.textEdit.redo)
+        self.actionCut.triggered.connect(self.textEdit.cut)
+        self.actionCopy.triggered.connect(self.textEdit.copy)
+        self.actionPaste.triggered.connect(self.textEdit.paste)
 
+        self.actionBold.triggered.connect(self.text_bold)
+        self.actionItalic.triggered.connect(self.italic)
+        self.actionUnderline.triggered.connect(self.underline)
+
+        self.actionLeft.triggered.connect(self.align_left)
+        self.actionDown.triggered.connect(self.align_center)
+        self.actionRight.triggered.connect(self.align_right)
+        self.actionJustify.triggered.connect(self.justify)
+
+        self.actionFont.triggered.connect(self.font_dialog)
+        self.actionColor.triggered.connect(self.color_dialog)
+        self.actionAbout_App.triggered.connect(self.about)
+
+# if the document has been changed and user tries to create a new document, this asks them if they
+# should save or discard the changes.
+    def maybe_save(self):
+        if not self.textEdit.document().isModified():
+            return True
+
+        ret = QMessageBox.warning(self, "Application",
+                                  "The document has been modified. \n Do you want to save it?",
+                                  QMessageBox.StandardButton.Save | QMessageBox.StandardButton.Discard | QMessageBox.StandardButton.Cancel)
+
+        if ret == QMessageBox.StandardButton.Save:
+            return self.save_file()
+
+        if ret == QMessageBox.StandardButton.Cancel:
+            return False
+
+        return True
+
+# save file
     def save_file(self):
         filename = QFileDialog.getSaveFileName(self, "Save File")
 
@@ -25,9 +74,91 @@ class NotePadWindow(QMainWindow, Ui_MainWindow):
 
         QMessageBox.about(self, "Save File", "File has been saved")
 
+    def print_file(self):
+        printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+        dialog = QPrintDialog(printer)
+
+        if dialog.exec() == QPrintDialog.DialogCode.Accepted:
+            self.textEdit.print(printer)
+
+    def preview_dialog(self):
+        printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+        previewDialog = QPrintPreviewDialog(printer, self)
+        previewDialog.paintRequested.connect(self.print_preview)
+        previewDialog.exec()
+
+    def print_preview(self, printer):
+        self.textEdit.print(printer)
+
+    def export_pdf(self):
+        fn, _ = QFileDialog.getSaveFileName(self, 'Export PDF', "PDF files (.pdf) ;; All Files()")
+
+        if fn != "":
+            if QFileInfo(fn).suffix() == "": fn += '.pdf'
+            printer = QPrinter(QPrinter.PrinterMode.HighResolution)
+            printer.setOutputFormat(QPrinter.OutputFormat.PdfFormat)
+            printer.setOutputFileName(fn)
+            self.textEdit.document().print(printer)
+
+# new file
+    def file_new(self):
+        if self.maybe_save():
+            self.textEdit.clear()
+
+    def open_file(self):
+        fname = QFileDialog.getOpenFileName(self, "Open File", '/home')
+
+        if fname[0]:
+            f = open(fname[0], 'r')
+
+            with f:
+                data = f.read()
+                self.textEdit.setText(data)
+
+    def exit_app(self):
+        self.close()
+
+    def text_bold(self):
+        font = QFont()
+        font.setBold(True)
+        self.textEdit.setFont(font)
+
+    def italic(self):
+        font = QFont()
+        font.setItalic(True)
+        self.textEdit.setFont(font)
+
+    def underline(self):
+        font = QFont()
+        font.setUnderline(True)
+        self.textEdit.setFont(font)
+
+    def align_left(self):
+        self.textEdit.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+    def align_center(self):
+        self.textEdit.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+    def align_right(self):
+        self.textEdit.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+    def justify(self):
+        self.textEdit.setAlignment(Qt.AlignmentFlag.AlignJustify)
+
+    def font_dialog(self):
+        font, ok = QFontDialog.getFont()
+
+        if ok:
+            self.textEdit.setFont(font)
+
+    def color_dialog(self):
+        color = QColorDialog.getColor()
+        self.textEdit.setTextColor(color)
+
+    def about(self):
+        QMessageBox.about(self, "About App", "This is a simple notepad app with PyQt6")
+
 
 app = QApplication(sys.argv)
 Note = NotePadWindow()
 sys.exit(app.exec())
-
-
